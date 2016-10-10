40264f322cbd4235dadfe9653c1d9ccd49b61ff1
Amitava Saha <amitava82@gmail.com>
2016-10-10 18:05:01 +0530

    remove helmet

--- client/app.js
+++ client/app.js
@@ -2,11 +2,9 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {push} from 'react-router-redux';
 import autobind from 'autobind-decorator';
-import Helmet from 'react-helmet';
 import {logout} from './redux/modules/session';
 import Toastr from './utils/toastr';
 import Header from './components/header';
-import Footer from './components/footer';
 import { events } from '../constants';
 
 
@@ -15,7 +13,7 @@ import './scss/export.scss';
 
 @connect(state => state)
 export default class App extends React.Component {
-    
+
     @autobind
     logout() {
         this.props.dispatch(logout());
